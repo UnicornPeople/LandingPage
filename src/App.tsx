3b1d08c38b003750f91bccd5ca9b99fe94fc624a
sungmin.jung <jspiner@naver.com>
2024-04-07 22:34:39 +0900

    SEO 적용, og태그 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Home from "./pages/home/Home";
 import Form from "./pages/form/Form";
 import Forms from "./pages/forms/Forms";
 import { useEffect } from "react";
+import { Helmet } from "react-helmet";
+import ogImg from "./resources/og.png";
 
 function App() {
   useEffect(() => {
@@ -16,14 +18,33 @@ function App() {
 
   });
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/form' element={<Form />} />
-        <Route path='/forms/:name' element={<Forms />} />
-        <Route path='/*' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+
+        <title>현직자에게 직접받는 모의면접, 스킬코치</title>
+        <meta name="description" content="네카라쿠배 현직자에게 직접 모의면접과 피드백을 받아보세요!" />
+        <meta name="keywords" content="네카라쿠배,개발자,면접,면접질문,모의면접,개발자 질문,프론트엔드,백엔드,디자인" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="현직자에게 직접받는 모의면접, 스킬코치" />
+        <meta property="og:site_name" content="현직자에게 직접받는 모의면접, 스킬코치" />
+        <meta property="og:description" content="네카라쿠배 현직자에게 직접 모의면접과 피드백을 받아보세요!" />
+        <meta property="og:image" content={ogImg} />
+        <meta property="og:url" content="https://skillcoach.co.kr" />
+
+      </Helmet>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/form' element={<Form />} />
+            <Route path='/forms/:name' element={<Forms />} />
+            <Route path='/*' element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
   );
 }
 
